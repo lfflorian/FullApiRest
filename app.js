754015de4719aeb8f1c0fa245fca758a47c40eac
luis <lfflorian4@gmail.com>
2018-11-22 21:06:30 -0600

    vista agregada

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const api = require('./routes')
+const hbs = require('express-handlebars');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use('/api', api)
 
+app.engine('.hbs', hbs({
+    defaultLayout: 'default',
+    extname: '.hbs'
+}))
+
+app.set('view engine', '.hbs')
+app.use('/api', api)
+app.get('/login', (req, res) => {
+    res.render('Login')
+})
 module.exports = app;
